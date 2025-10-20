b6f0be98827fa4474e8249b07fc9d29e9289cfa8
Gustavo Castro <gustavoprasca@hotmail.com>
2025-10-20 14:43:00 -0500

    refactor

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into product(id, name, price) values(1, 'computer', 400);
 insert into product(id, name, price) values(2, 'mouse', 10);
 
-insert into customer(id, username, password, authorities) values(1, 'gcastro', '$2a$12$4hSBf5jrPEWXA1yVFq6ND.hM0FHsY7XNQvXkmI2uy8lQBsRM/Bqey', 'ROLE_admin');
-insert into customer(id, username, password, authorities) values(2, 'test', '$2a$12$UXd9ZFwzZcH4kXLiV0BVveXXIOEGLGix06nZq6hlsoM9Q.eScpkW.', 'ROLE_user');
+insert into customer(id, username, password, authorities) values(1, 'gcastro', '$2a$12$LWsuxX.8DmQPtsRaFiZe5edLeQBBAk5tiPL6C1jRJZfFKAvvt5cCS', 'ROLE_admin');
+insert into customer(id, username, password, authorities) values(2, 'test', '$2a$12$LWsuxX.8DmQPtsRaFiZe5edLeQBBAk5tiPL6C1jRJZfFKAvvt5cCS', 'ROLE_user');
